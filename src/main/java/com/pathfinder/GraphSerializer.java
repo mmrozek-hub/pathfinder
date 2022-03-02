d2370790c976053d1fc17695782d54e7ea8c8304
mmrozek-hub <https://mmrozek-hub@github.com>
2022-03-02 20:03:21 +0100

    init commit - spring boot bootstrap

---
+++ src/main/java/com/pathfinder/GraphSerializer.java
@@ -0,0 +1,41 @@
+package com.pathfinder;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+public class GraphSerializer {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println(createStringGraph().toString());
+		
+	}
+	
+	
+    private static Graph<String, DefaultEdge> createStringGraph()
+    {
+        Graph<String, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
+      
+
+        String v1 = "v1";
+        String v2 = "v2";
+        String v3 = "v3";
+        String v4 = "v4";
+
+        // add the vertices
+        g.addVertex(v1);
+        g.addVertex(v2);
+        g.addVertex(v3);
+        g.addVertex(v4);
+
+        // add edges to create a circuit
+        g.addEdge(v1, v2);
+        g.addEdge(v2, v3);
+        g.addEdge(v3, v4);
+        g.addEdge(v4, v1);
+
+        return g;
+    }
+}
