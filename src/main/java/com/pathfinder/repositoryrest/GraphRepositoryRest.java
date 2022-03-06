8eb61e460790cf8923487a4fbf76d9f62d43a0a4
mmrozek-hub <https://mmrozek-hub@github.com>
2022-03-06 22:07:36 +0100

    crud rest service instead of paged one, setting port to allow service and consumer apps working at once

--- src/main/java/com/pathfinder/repositoryrest/GraphRepositoryRest.java
+++ src/main/java/com/pathfinder/repositoryrest/GraphRepositoryRest.java
@@ -2,14 +2,14 @@ package com.pathfinder.repositoryrest;
 
 import java.util.List;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.pathfinder.model.Graph;
 
 @RepositoryRestResource(collectionResourceRel = "graphs", path = "graphs")
-public interface GraphRepositoryRest extends PagingAndSortingRepository<Graph, Long> {
+public interface GraphRepositoryRest extends CrudRepository<Graph, Long> {
 
 	List<Graph> findByName(@Param("name") String name );
 
